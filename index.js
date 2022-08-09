e1eef0da607ea234aebd5195e04f170a19e69fa0
himubarua1 <87941533+himubarua1@users.noreply.github.com>
2022-08-09 06:24:17 +0600

    its my socket.io practice file

---
+++ index.js
@@ -0,0 +1,77 @@
+const express = require('express');
+const app = express();
+const http = require('http');
+const expressServer = http.createServer(app);
+
+
+
+
+const {Server} = require('socket.io');
+const io = new Server(expressServer);
+
+
+// DATA SEND SERVER TO CLIENT
+
+
+// io.on('connection', function(socket){
+//     console.log('New User Connected')
+
+//     setInterval(function(){
+//         let d = new Date();
+//         let t = d.getTime();
+//         socket.emit("myEvent", t); 
+//     }, 500)
+
+// });
+
+
+// DATA RCVD CLIENT FROM SERVER
+
+
+// io.on('connection', function(socket){
+//     console.log('New User Connected')
+//     socket.on('message', function(msg){
+//         console.log(msg);
+//     })
+// })
+
+
+// BROADCAST
+
+
+// io.on('connection', function(socket){
+//     io.sockets.emit("MyBroadcast", "Hello Everyone !")
+// })
+
+
+
+// NAMESPACE
+
+
+
+let buyNsp = io.of("/buy");
+
+buyNsp.on('connection', function(socket){
+
+    buyNsp.emit("MyEvent", "hello buy")
+});
+
+let sellNsp = io.of("/sell");
+
+sellNsp.on('connection', function(socket){
+
+    sellNsp.emit("MyEvent", "hello sell")
+});
+
+
+
+
+app.get('/', function(req,res){
+   res.sendFile(__dirname+"/index.html")
+});
+
+
+
+expressServer.listen(3050, function(){
+    console.log("Server run @ 3000")
+});
