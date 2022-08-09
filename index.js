1463e3528db600c7fde74067a49d88f78cfbc758
himubarua1 <87941533+himubarua1@users.noreply.github.com>
2022-08-09 08:00:50 +0600

    namespace & rooms practice

--- index.js
+++ index.js
@@ -2,52 +2,43 @@ const express = require('express');
 const app = express();
 const http = require('http');
 const expressServer = http.createServer(app);
-
-
-
-
 const {Server} = require('socket.io');
 const io = new Server(expressServer);
 
 
 // DATA SEND SERVER TO CLIENT
 
+io.on('connection', function(socket){
+    console.log('New User Connected')
 
-// io.on('connection', function(socket){
-//     console.log('New User Connected')
+    setInterval(function(){
+        let d = new Date();
+        let t = d.getTime();
+        socket.emit("myEvent", t); 
+    }, 500)
 
-//     setInterval(function(){
-//         let d = new Date();
-//         let t = d.getTime();
-//         socket.emit("myEvent", t); 
-//     }, 500)
-
-// });
+});
 
 
 // DATA RCVD CLIENT FROM SERVER
 
-
-// io.on('connection', function(socket){
-//     console.log('New User Connected')
-//     socket.on('message', function(msg){
-//         console.log(msg);
-//     })
-// })
+io.on('connection', function(socket){
+    console.log('New User Connected')
+    socket.on('message', function(msg){
+        console.log(msg);
+    })
+})
 
 
 // BROADCAST
 
-
-// io.on('connection', function(socket){
-//     io.sockets.emit("MyBroadcast", "Hello Everyone !")
-// })
+io.on('connection', function(socket){
+    io.sockets.emit("MyBroadcast", "Hello Everyone !")
+})
 
 
 
 // NAMESPACE
-
-
 
 let buyNsp = io.of("/buy");
 
@@ -62,6 +53,32 @@ sellNsp.on('connection', function(socket){
 
     sellNsp.emit("MyEvent", "hello sell")
 });
+
+
+
+// ROOMS
+
+io.on('connection', function(socket){
+    socket.join('kitchen-room');
+
+    let sizeOfKitchen = io.sockets.adapter.rooms.get('kitchen-room').size;
+
+    io.sockets.in('kitchen-room').emit('cooking',"Fried Rice cooking =" + sizeOfKitchen)
+    io.sockets.in('kitchen-room').emit('boiling',"Boiling Water")
+
+
+
+    socket.join('bed-room');
+    io.sockets.in('bed-room').emit('sleep',"i am sleeping")
+    io.sockets.in('bed-room').emit('rest',"i am taking rest")
+})
+
+
+
+
+
+
+
 
 
 
